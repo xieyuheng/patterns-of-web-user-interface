469e4245f8cde5ff5b78eb4023fb864d84e576aa
Xie Yuheng <xyheme@gmail.com>
2023-02-11 00:15:10 +0800

    up

--- src/pages/Dropdown.vue
+++ src/pages/Dropdown.vue
@@ -3,11 +3,11 @@ import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue"
 </script>
 
 <template>
-  <div class="flex mt-12 w-full items-center justify-center">
+  <div class="mt-12 flex w-full items-center justify-center">
     <Menu as="div" class="flex flex-col">
       <MenuButton class="border border-black p-3 text-left">More</MenuButton>
       <div class="relative">
-        <MenuItems class="absolute top-2 right-0 flex flex-col min-w-max">
+        <MenuItems class="absolute top-2 right-0 flex min-w-max flex-col">
           <MenuItem v-slot="{ active }" class="border border-black p-3">
             <a :class="{ 'bg-stone-200': active }"> Account settings </a>
           </MenuItem>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,8 +3,10 @@ import Link from "../components/Link.vue"
 </script>
 
 <template>
-  <div class="flex flex-col space-y-2 p-3">
-    <Link class="font-bold hover:underline" href="/dropdown"> Dropdown </Link>
-    <Link class="font-bold hover:underline" href="/modal"> Modal </Link>
+  <div class="flex flex-col items-center">
+    <div class="mt-12 flex max-w-max flex-col space-y-2">
+      <Link class="border border-black p-3" href="/dropdown"> Dropdown </Link>
+      <Link class="border border-black p-3" href="/modal"> Modal </Link>
+    </div>
   </div>
 </template>
